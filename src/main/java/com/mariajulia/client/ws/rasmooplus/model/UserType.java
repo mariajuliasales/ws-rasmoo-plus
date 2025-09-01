5d569e6513eb60de06ed2365c28a54edba78bfb5
mariajuliasales <salesmariajulia11@gmail.com>
2025-09-01 18:15:46 -0300

    feat: cria entidade UserType

---
+++ src/main/java/com/mariajulia/client/ws/rasmooplus/model/UserType.java
@@ -0,0 +1,28 @@
+package com.mariajulia.client.ws.rasmooplus.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.io.Serializable;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "user_type")
+public class UserType implements Serializable {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "user_type_id")
+    private long id;
+
+    private String name;
+
+    private String description;
+
+}
