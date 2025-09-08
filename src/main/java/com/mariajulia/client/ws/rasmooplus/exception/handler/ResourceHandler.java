99bd062fd915331f9922824982140add7839266f
mariajuliasales <salesmariajulia11@gmail.com>
2025-09-08 15:53:25 -0300

    feat: adiciona resourcehandler para tratar erros de validação de DTO e retornar resposta personalizada

--- src/main/java/com/mariajulia/client/ws/rasmooplus/exception/handler/ResourceHandler.java
+++ src/main/java/com/mariajulia/client/ws/rasmooplus/exception/handler/ResourceHandler.java
@@ -4,8 +4,13 @@ import com.mariajulia.client.ws.rasmooplus.dto.error.ErrorResponse;
 import com.mariajulia.client.ws.rasmooplus.exception.NotFoundException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.util.Map;
+import java.util.stream.Collectors;
 
 @RestControllerAdvice
 public class ResourceHandler {
@@ -18,6 +23,20 @@ public class ResourceHandler {
                 .httpStatus(HttpStatus.NOT_FOUND)
                 .statusCode(HttpStatus.NOT_FOUND.value())
                 .build());
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<ErrorResponse> methodArgumentNotValidException(MethodArgumentNotValidException ex) {
+        Map<String, String> messages = ex.getBindingResult().getFieldErrors().stream()
+                .collect(Collectors.toMap(FieldError::getField, FieldError::getDefaultMessage));
+
+        ErrorResponse errorResponse = ErrorResponse.builder()
+                .message(messages.toString())
+                .httpStatus(HttpStatus.BAD_REQUEST)
+                .statusCode(HttpStatus.BAD_REQUEST.value())
+                .build();
+
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(errorResponse);
     }
 
 }
