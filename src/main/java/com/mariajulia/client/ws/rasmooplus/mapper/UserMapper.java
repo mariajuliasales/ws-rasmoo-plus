57c9200eae5a0c347178bcc5bb111ef6230181cf
mariajuliasales <salesmariajulia11@gmail.com>
2025-09-09 19:00:35 -0300

    feat: adiciona definição automática de dtExpiration ao criar User

--- src/main/java/com/mariajulia/client/ws/rasmooplus/mapper/UserMapper.java
+++ src/main/java/com/mariajulia/client/ws/rasmooplus/mapper/UserMapper.java
@@ -8,15 +8,19 @@ import com.mariajulia.client.ws.rasmooplus.model.User;
 import com.mariajulia.client.ws.rasmooplus.model.UserType;
 import lombok.experimental.UtilityClass;
 
+import java.time.LocalDate;
+
 @UtilityClass
 public class UserMapper {
 
     public static User toUser(UserRequest request, UserType userType, SubscriptionType subscriptionType) {
+        LocalDate today = LocalDate.now();
         return User.builder()
                 .name(request.name())
                 .email(request.email())
                 .phone(request.phone())
                 .cpf(request.cpf())
+                .dtExpiration(today.plusYears(1))
                 .userType(userType)
                 .subscriptionType(subscriptionType)
                 .build();
